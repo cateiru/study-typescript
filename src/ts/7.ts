c2e0bac3502466f5a05530440a1bcabe39d0d8e8
yuto51942 <yuto.w51942@gmail.com>
2020-07-15 16:00:22 +0900

    solidity 変換の研究

---
+++ src/ts/7.ts
@@ -0,0 +1,70 @@
+/*
+    引数の情報からsolidityのプログラムを生成する。
+
+*/
+
+export interface Node {
+    name: string,
+    opType: string,
+    inputs: ReadonlyArray<number>,
+    outputs: ReadonlyArray<number>
+}
+
+export interface Input {
+    name: string,
+    outputs: ReadonlyArray<number>
+}
+
+export interface Output {
+    name: string,
+    outputs: ReadonlyArray<number>
+}
+
+export interface Data {
+    inputs: ReadonlyArray<Input>,
+    output: Output,
+    nodes: ReadonlyArray<Node>,
+
+}
+
+export function convert(data: Data): string[] {
+    // TODO: 入力すべてに対しsolidityの変数宣言をする。
+    // TODO: その変数に入力するようなfunctionを作成
+    // TODO: outputの変数作成
+    // TODO: 計算
+
+    const contractName: string = 'Calculation';
+    const inputFunctionName: string = 'input_';
+    const evaluateFunctionName: string = 'evaluate';
+    const calculate: string = 'a + b';
+
+
+    var solidity = ['pragma solidity ^0.6.9;', ''];
+
+    solidity.push(`contract ${contractName} {`)
+
+    for(let element of data.inputs){
+        solidity.push(`  int256 public ${element.name};`);
+    }
+
+    for(let element of data.inputs){
+        solidity.push(
+            '',
+            `  function ${inputFunctionName + element.name}(int256 input_number) public {`,
+            `    ${element.name} = input_number;`,
+            '  }'
+            )
+    }
+
+    solidity.push(
+        '',
+        `  function ${evaluateFunctionName}() public view returns (int256) {`,
+        `    return ${calculate};`,
+        '  }'
+    )
+
+    solidity.push('}');
+
+    return solidity;
+
+}
---
+++ src/ts/7_run.ts
@@ -0,0 +1,41 @@
+import {Data, Node, Input, Output, convert} from './7';
+import * as fs from 'fs';
+
+function main(): void {
+
+    const node1: Node = {
+        name: 'tf_op_layer_add/add',
+        opType: 'Add',
+        inputs: [1, 2],
+        outputs: [3]
+    }
+
+    const input1: Input = {
+        name: 'a',
+        outputs: [0]
+    }
+
+    const input2: Input = {
+        name: 'b',
+        outputs: [1]
+    }
+
+    const output: Output = {
+        name: 'tf_op_layer_truediv',
+        outputs: [3]
+    }
+
+    const inputData: Data = {
+        inputs: [input1, input2],
+        output: output,
+        nodes: [node1]
+    }
+
+    const path = 'test.sol';
+    var solidity: string[] = convert(inputData);
+
+    console.log(solidity);
+    fs.writeFileSync(path, solidity.join('\n'), 'utf-8');
+}
+
+main();
