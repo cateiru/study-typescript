5d729787de8917a7f9537f4231e999f82e9bb74e
yuto51942 <yuto.w51942@gmail.com>
2020-08-07 11:03:16 +0900

    added 10

---
+++ src/ts/10.ts
@@ -0,0 +1,13 @@
+function main(): void {
+    const num: number = 10;
+
+    console.log(`number of ${num}`);
+
+    const li: string[] = ['Hello', 'World'];
+
+    for (const listIndex in li){
+        console.log(`index of ${listIndex}`);
+    }
+}
+
+main()
